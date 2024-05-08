3929911a776ff6536be59d813fa4d5f90599cdbe
Mitch Boling <mphboling@gmail.com>
2024-05-07 20:05:42 -0400

    Created Vehicle POCO and Repo

--- VehicleRepository/VehicleRepo.cs
+++ VehicleRepository/VehicleRepo.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Vehicle.Repository;
+
+public class VehicleRepository
+{
+    private List<Vehicle> _vehicleList = new List<Vehicle>();
+
+    // Create
+
+    public void AddVehicleToList(Vehicle car)
+    {
+        _vehicleList.Add(car);
+    }
+
+    // Read
+
+    public List<Vehicle> GetVehicleList()
+    {
+        return new List<Vehicle>(_vehicleList);
+    }
+
+    // Update
+
+    public bool UpdateExistingVehicles(string originalModel, Vehicle newCar)
+    {
+        Vehicle oldCar = GetVehicleByModel(originalModel);
+
+        if (oldCar != null)
+        {
+            oldCar.CarMake = newCar.CarMake;
+            oldCar.CarModel = newCar.CarModel;
+            oldCar.CarYear = newCar.CarYear;
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Delete
+
+    public bool RemoveVehicleFromList(string carmodel)
+    {
+        Vehicle car = GetVehicleByModel(carmodel);
+
+        if (car == null)
+        {
+            return false;
+        }
+
+        int initialCount = _vehicleList.Count;
+        _vehicleList.Remove(car);
+
+        if (initialCount > _vehicleList.Count)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+    // Helper method
+
+    public Vehicle GetVehicleByModel(string carmodel)
+    {
+        foreach (Vehicle car in _vehicleList)
+        {
+            if (car.CarModel == carmodel)
+            {
+                return car;
+            }
+        }
+
+        return null;
+    }
+}
